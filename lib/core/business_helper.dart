e37a414ffd3caaf539b8fae6515e5e904190e745
jecastanosm <jecastanosm@concredito.com.mx>
2026-02-16 21:50:37 -0700

    fix null crash

--- lib/core/business_helper.dart
+++ lib/core/business_helper.dart
@@ -2,12 +2,27 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 Future<String> getBusinessId() async {
-  final uid = FirebaseAuth.instance.currentUser!.uid;
+
+  final user = FirebaseAuth.instance.currentUser;
+
+  if (user == null) {
+    throw Exception("Usuario no autenticado");
+  }
 
   final doc = await FirebaseFirestore.instance
       .collection('users')
-      .doc(uid)
+      .doc(user.uid)
       .get();
 
-  return doc['businessId'];
+  if (!doc.exists) {
+    throw Exception("Documento de usuario no existe");
+  }
+
+  final data = doc.data();
+
+  if (data == null || data['businessId'] == null) {
+    throw Exception("Usuario sin negocio asignado");
+  }
+
+  return data['businessId'] as String;
 }
